75f499d496b85899dc7f53bb4c7e0d5caba6fe9c
anttorre <antonio.torre@nttdata.com>
2023-06-26 16:41:28 +0200

    IDP-1106 findById optimize to be run on cosmosDB

--- src/main/java/it/gov/pagopa/UserRoleApplication.java
+++ src/main/java/it/gov/pagopa/UserRoleApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "it.gov.pagopa")
 public class UserRoleApplication {
 
     public static void main(String[] args) {
---
+++ src/test/java/it/gov/pagopa/common/mongo/repository/BaseMongoRepositoryTestIntegrated.java
@@ -0,0 +1,34 @@
+package it.gov.pagopa.common.mongo.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.TestPropertySource;
+
+/**
+ * See confluence page: <a href="https://pagopa.atlassian.net/wiki/spaces/IDPAY/pages/615974424/Secrets+UnitTests">Secrets for UnitTests</a>
+ */
+@SuppressWarnings({"squid:S3577", "NewClassNamingConvention"}) // suppressing class name not match alert: we are not using the Test suffix in order to let not execute this test by default maven configuration because it depends on properties not pushable. See
+@TestPropertySource(locations = {
+        "classpath:/mongodbEmbeddedDisabled.properties",
+        "classpath:/secrets/mongodbConnectionString.properties"
+})
+class BaseMongoRepositoryTestIntegrated extends BaseMongoRepositoryIntegrationTest {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Override
+    @Test
+    void testFindById() {
+        super.testFindById();
+
+        org.bson.Document result = mongoTemplate.executeCommand(new org.bson.Document("getLastRequestStatistics", 1));
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals("find", result.get("CommandName"));
+        double ru = (double) result.get("RequestCharge");
+        Assertions.assertTrue(ru <= 4.0, "Unexpected RU consumed! " + ru);
+    }
+}
