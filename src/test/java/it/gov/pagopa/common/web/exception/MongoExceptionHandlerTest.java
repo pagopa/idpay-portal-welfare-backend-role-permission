c011b3bb2720e1608204a73ca2cd0354438e7a02
anttorre <antonio.torre@nttdata.com>
2023-09-12 14:31:39 +0200

    IDP-1765 selected API retryable

--- src/test/java/it/gov/pagopa/common/web/exception/MongoExceptionHandlerTest.java
+++ src/test/java/it/gov/pagopa/common/web/exception/MongoExceptionHandlerTest.java
@@ -71,7 +71,6 @@ class MongoExceptionHandlerTest {
     mockMvc.perform(MockMvcRequestBuilders.get("/test")
                     .contentType(MediaType.APPLICATION_JSON))
             .andExpect(MockMvcResultMatchers.status().isTooManyRequests())
-            .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("TOO_MANY_REQUESTS"))
             .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("TOO_MANY_REQUESTS"))
             .andExpect(MockMvcResultMatchers.header().exists(HttpHeaders.RETRY_AFTER))
             .andExpect(MockMvcResultMatchers.header().string(HttpHeaders.RETRY_AFTER, "1"))
