e4e2079e2be4efeb2fc349032a7dff4beb94479f
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2023-10-26 11:24:33 +0200

    fix: Sonar fix (#59)
    Co-authored-by: frtrinca <francesco.trinca@nttdata.com>

--- src/main/java/it/gov/pagopa/config/SpringDocConfiguration.java
+++ src/main/java/it/gov/pagopa/config/SpringDocConfiguration.java
@@ -1,9 +1,7 @@
 package it.gov.pagopa.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
