6f31884bb11a502d77a3bf381ee0009b451da149
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2024-01-08 16:44:14 +0100

    IDP-2253-remove-http-errors-from-service

--- src/main/java/it/gov/pagopa/role/permission/UserRoleApplication.java
+++ src/main/java/it/gov/pagopa/role/permission/UserRoleApplication.java
@@ -1,4 +1,4 @@
-package it.gov.pagopa;
+package it.gov.pagopa.role.permission;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
